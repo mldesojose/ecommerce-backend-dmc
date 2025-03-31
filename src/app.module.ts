ef5925b62c6af54926f59298562a98f09dcb6b35
Jose David Lagos Morales <mldesojose@gmail.com>
2025-03-31 02:44:28 -0500

    configurar variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,19 @@ import { LoginModule } from './login/login.module';
 import { SalesModule } from './sales/sales.module';
 import { ProductsModule } from './products/products.module';
 import { UsuarioModule } from './usuario/usuario.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [LoginModule, SalesModule, ProductsModule, UsuarioModule],
+  imports: [
+    LoginModule, 
+    SalesModule, 
+    ProductsModule, 
+    UsuarioModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Hace que las variables estén disponibles globalmente
+      envFilePath: '.env', // Ruta al archivo .env (opcional)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
